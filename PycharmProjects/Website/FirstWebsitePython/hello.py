3485b4e6036ecd5b65642e7213e7383bfa09f7ef
Jacob Chaar <jacobchaar@live.ca>
2017-11-19 16:17:16 -0500

    testing flask module

--- PycharmProjects/Website/FirstWebsitePython/hello.py
+++ PycharmProjects/Website/FirstWebsitePython/hello.py
@@ -5,8 +5,19 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return "Hey There!"
+@app.route('/index')
+def index():
+    user = {"First name": "Jacob"}
+    return """
+    <html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello,""" + user['First name'] + """</h1>
+  </body>
+</html>
+    """
 
 if __name__=="__main__":
     app.run(debug=True)
